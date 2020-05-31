a8ad6801b46751b5c009631e3f59b07bf09289e4
Vincent Battaglia <vpbattaglia@gmail.com>
2020-05-30 18:07:00 -0700

    uibuild

---
+++ build/precache-manifest.870cf277f9a8df5e10ec4d7294611076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc0e6f806c9c5488f1c5bf6b05a11e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f69b998b546b5783a7e",
+    "url": "/static/css/main.87be4573.chunk.css"
+  },
+  {
+    "revision": "0b2105cad82ad22bed6a",
+    "url": "/static/js/2.6d85c828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d85c828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f69b998b546b5783a7e",
+    "url": "/static/js/main.ce5745e9.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
